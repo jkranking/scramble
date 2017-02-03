6be27f1edddb4f03fc1933a9170f1cac2bce0523
Ian Peter Greenburg & Jake Kelly <pair+ianpetergreenburg.jakemkelly@devbootcamp.com>
2017-02-02 16:42:02 -0800

    successfully creating trips

--- app/assets/javascripts/trip_controller.js
+++ app/assets/javascripts/trip_controller.js
@@ -17,6 +17,16 @@ function initMap(){
 
     $('#submit-pings').click(function handler(event) {
       event.preventDefault()
+
+      $.post({
+        url: "/trips",
+        data: {trip: {latitude: trip.model.center_lat,
+                      longitude: trip.model.center_lng,
+                      zoom: trip.model.zoom,
+                      user_id: 1}}
+      }).success(function(response){
+        alert(response)
+      })
       google.maps.event.removeListener(latLng);
       $('#submit-pings').hide()
       $('#add-ping').show()
