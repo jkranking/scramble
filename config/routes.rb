f5ac0d4989f5251c19fdb2550bc17259d95c1196
Jeffrey Kranking & Warner Nielsen <pair+jkranking.warniel08@devbootcamp.com>
2017-02-02 19:10:52 -0800

    adds user profile route, controller, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+
+  get '/users/:id', to: 'users#show', as: 'user'
+
   root 'trips#index'
 
   resources :trips
