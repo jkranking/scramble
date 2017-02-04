295f0063d09ea9a0426f5e548390edc79cde8764
Ian Peter Greenburg & Jake Kelly <pair+ianpetergreenburg.jakemkelly@devbootcamp.com>
2017-02-03 17:19:07 -0800

    typo in Static Helper, ping.long to ping.lng

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -2,7 +2,7 @@ module StaticHelper
   def static_path(trip)
     path = []
     trip.pings.each do |ping|
-      path << "#{ping.lat},#{ping.long}"
+      path << "#{ping.lat},#{ping.lng}"
     end
     path = path.join('|')
   end
