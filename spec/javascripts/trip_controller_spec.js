873a2678073cce0dd14f3eb644a53bd6608f122c
Ian Peter Greenburg & Jake Kelly <pair+ianpetergreenburg.jakemkelly@devbootcamp.com>
2017-02-05 13:12:27 -0800

    jasmine now configured

--- spec/javascripts/trip_controller_spec.js
+++ spec/javascripts/trip_controller_spec.js
@@ -1,6 +1,9 @@
+console.log(  'test is running')
+
 describe('TripController', function(){
+  before each
   it('does a thing', function(){
-    trip = new TripController(new TripView, new TripModel)
+    trip = new TripController(new TripView, new TripModel(map))
     expect(trip).toBe(TripController)
-  }
-}
+  })
+})
