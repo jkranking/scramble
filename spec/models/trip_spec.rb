4506c039acad51cafbc73a29605f463d1baf13ca
Jake Kelly <email": null>
2017-02-05 15:01:03 -0800

    Add trip spec changes

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -5,10 +5,18 @@ RSpec.describe Trip, type: :model do
   it { is_expected.to validate_presence_of :latitude }
   it { is_expected.to validate_presence_of :longitude }
   it { is_expected.to validate_presence_of :user_id }
+  it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_numericality_of :zoom }
   it { is_expected.to validate_numericality_of :latitude }
   it { is_expected.to validate_numericality_of :longitude }
   it { is_expected.to validate_numericality_of :user_id }
   it { is_expected.to have_many :markers }
   it { is_expected.to have_many :pings }
+  it { is_expected.to belong_to :user }
+
+  it "orders markers by created at" do
+    let (:)
+    expect
+  end
+  end
 end
