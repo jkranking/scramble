a28ca9951ec3a967e4c2eac4fe107f5b1567b138
Jeffrey Kranking <email": null>
2017-02-05 19:30:24 -0800

    adds feature testing for homepage and trip show

---
+++ spec/features/visiting_the_homepage_feature_spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+
+feature 'visiting the homepage' do
+  scenario 'the visitor sees a list of trips and can view them' do
+    trip = FactoryGirl.create(:trip)
+    visit root_path
+    within('.trip-list') do
+      expect(page).to have_content trip.name
+      click_link(trip.try(:name))
+    end
+    expect(page).to have_current_path trip_path(trip)
+  end
+
+  scenario 'the visitor can go to the login page' do
+    visit root_path
+    within('.navbar-right') do
+      expect(page).to have_content "Login"
+      click_link("Login")
+    end
+    expect(page).to have_current_path new_user_session_path
+  end
+
+  scenario 'the visitor can go to the sign up page' do
+    visit root_path
+    within('.navbar-right') do
+      expect(page).to have_content "Sign up"
+      click_link("Sign up")
+    end
+    expect(page).to have_current_path new_user_registration_path
+  end
+
+  scenario 'the visitor can click the home page button' do
+    visit root_path
+    within('.navbar-left') do
+      expect(page).to have_content "Scramble"
+      click_link("Scramble")
+    end
+    expect(page).to have_current_path trips_path
+  end
+
+  scenario 'if logged in, the user can logout' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
+    visit root_path
+    within('.navbar-right') do
+      expect(page).to have_content "Logout"
+      click_link("Logout")
+    end
+    expect(page).to have_current_path "/"
+  end
+
+  scenario 'if logged in, the user can create a new trip' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
+    visit root_path
+    within('.navbar-right') do
+      expect(page).to have_content "Logout"
+      click_link("Logout")
+    end
+    expect(page).to have_current_path "/"
+  end
+
+  scenario 'if logged in, the user can view their profile' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
+    visit root_path
+    within('.navbar-right') do
+      expect(page).to have_content "Profile"
+      click_link("Profile")
+    end
+    expect(page).to have_current_path user_path(user)
+  end
+
+  scenario 'if logged in, the user can view their trips' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
+    visit root_path
+    within('.navbar-right') do
+      expect(page).to have_content "My Trips"
+      click_link("My Trips")
+    end
+    expect(page).to have_current_path user_trips_path(user)
+  end
+
+end
+
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,10 @@ require 'devise'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.after :each do
+    Warden.test_reset!
+  end
+
   config.include Devise::Test::ControllerHelpers, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
