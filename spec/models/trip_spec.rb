d1316bc39d894e5bad46a1006dc73a23288160fb
Warner Nielsen <bwn888@gmail.com>
2017-02-06 08:55:39 -0800

    Format marker, ping, and trip model specs to look a little cleaner and to divide the tests into validations and associations

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  it { is_expected.to validate_presence_of :zoom }
-  it { is_expected.to validate_presence_of :latitude }
-  it { is_expected.to validate_presence_of :longitude }
-  it { is_expected.to validate_presence_of :user_id }
-  it { is_expected.to validate_numericality_of :zoom }
-  it { is_expected.to validate_numericality_of :latitude }
-  it { is_expected.to validate_numericality_of :longitude }
-  it { is_expected.to validate_numericality_of :user_id }
-  it { is_expected.to have_many :markers }
-  it { is_expected.to have_many :pings }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :zoom }
+    it { is_expected.to validate_presence_of :latitude }
+    it { is_expected.to validate_presence_of :longitude }
+    it { is_expected.to validate_presence_of :user_id }
+    it { is_expected.to validate_numericality_of :zoom }
+    it { is_expected.to validate_numericality_of :latitude }
+    it { is_expected.to validate_numericality_of :longitude }
+    it { is_expected.to validate_numericality_of :user_id }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many :markers }
+    it { is_expected.to have_many :pings }
+  end
 end
